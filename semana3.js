cbbe7c8a6c532577d20bda3ddfed00d73f2d394d
AnaliceMarques <analicemarquessl@gmail.com>
2023-01-20 03:47:11 -0300

    Modulo 1 - Semana 6

--- semana3.js
+++ semana3.js
@@ -34,7 +34,7 @@ const produto3 = {
   imagem: "./media/Leite-Condensado-Integral-Moça-Nestle-395g-300x300.png",
 };
 
-//Produto 4 (objeto)
+//Produto 4 (objeto) - teste
 const produto4 = {
   categoria: "detergente",
   nome: "Detergente Líquido Neutro",
@@ -53,6 +53,21 @@ console.log(produtosDisponiveis);
 
 //Relatório dos Produtos
 
+for (let i = 0; i < produtosDisponiveis.length; i++) {
+  console.log(`${produtosDisponiveis[i].categoria.toUpperCase()}
+Nome: ${produtosDisponiveis[i].nome}
+Embalagem: ${produtosDisponiveis[i].embalagem}
+Marca: ${produtosDisponiveis[i].marca}
+Preço: ${produtosDisponiveis[i].preco.toFixed(2)}
+O preço está com desconto? ${produtosDisponiveis[i].desconto}
+Produtos complementares: ${produtosDisponiveis[i].produtosComplementares.join(
+    ", "
+  )}`);
+  console.log("\n");
+}
+
+/* 
+//Outra opção de fazer o item 1
 //Produto 1
 let textoProdutosComplementares1 = "";
 for (let chave in produtosDisponiveis[0]) {
@@ -60,13 +75,6 @@ for (let chave in produtosDisponiveis[0]) {
     textoProdutosComplementares1 = produtosDisponiveis[0][chave];
   }
 }
-console.log(`${produtosDisponiveis[0].categoria.toUpperCase()}
-Nome: ${produtosDisponiveis[0].nome}
-Embalagem: ${produtosDisponiveis[0].embalagem}
-Marca: ${produtosDisponiveis[0].marca}
-Preço: ${produtosDisponiveis[0].preco.toFixed(2)}
-O preço está com desconto? ${produtosDisponiveis[0].desconto}
-Produtos complementares: ${textoProdutosComplementares1.join(", ")}`);
 
 //Produto 2
 let textoProdutosComplementares2 = "";
@@ -75,13 +83,6 @@ for (let chave in produtosDisponiveis[1]) {
     textoProdutosComplementares2 = produtosDisponiveis[1][chave];
   }
 }
-console.log(`${produtosDisponiveis[1].categoria.toUpperCase()}
-Nome: ${produtosDisponiveis[1].nome}
-Embalagem: ${produtosDisponiveis[1].embalagem}
-Marca: ${produtosDisponiveis[1].marca}
-Preço: ${produtosDisponiveis[1].preco.toFixed(2)}
-O preço está com desconto? ${produtosDisponiveis[1].desconto}
-Produtos complementares: ${textoProdutosComplementares2.join(", ")}`);
 
 //Produto 3
 let textoProdutosComplementares3 = "";
@@ -90,6 +91,23 @@ for (let chave in produtosDisponiveis[2]) {
     textoProdutosComplementares3 = produtosDisponiveis[2][chave];
   }
 }
+
+console.log(`${produtosDisponiveis[0].categoria.toUpperCase()}
+Nome: ${produtosDisponiveis[0].nome}
+Embalagem: ${produtosDisponiveis[0].embalagem}
+Marca: ${produtosDisponiveis[0].marca}
+Preço: ${produtosDisponiveis[0].preco.toFixed(2)}
+O preço está com desconto? ${produtosDisponiveis[0].desconto}
+Produtos complementares: ${textoProdutosComplementares1.join(", ")}`);
+
+console.log(`${produtosDisponiveis[1].categoria.toUpperCase()}
+Nome: ${produtosDisponiveis[1].nome}
+Embalagem: ${produtosDisponiveis[1].embalagem}
+Marca: ${produtosDisponiveis[1].marca}
+Preço: ${produtosDisponiveis[1].preco.toFixed(2)}
+O preço está com desconto? ${produtosDisponiveis[1].desconto}
+Produtos complementares: ${textoProdutosComplementares2.join(", ")}`);
+
 console.log(`${produtosDisponiveis[2].categoria.toUpperCase()}
 Nome: ${produtosDisponiveis[2].nome}
 Embalagem: ${produtosDisponiveis[2].embalagem}
@@ -97,6 +115,7 @@ Marca: ${produtosDisponiveis[2].marca}
 Preço: ${produtosDisponiveis[2].preco.toFixed(2)}
 O preço está com desconto? ${produtosDisponiveis[2].desconto}
 Produtos complementares: ${textoProdutosComplementares3.join(", ")}`);
+*/
 
 //Relatório de todos os produtos
 for (produto of produtosDisponiveis) {
@@ -122,8 +141,8 @@ console.log(relatorio(produto2));
 console.log(`\n`);
 console.log(relatorio(produto3));
 
-//Função para verificar se um produto está dentro de uma lista
-
+//Função para verificar se um produto está dentro de uma lista, retorna um objeto
+/*
 function verificaProdutoNaLista(listaDeProdutos, nomeDoProduto) {
   let produtoEncontrado = "";
 
@@ -147,12 +166,12 @@ function verificaProdutoNaLista(listaDeProdutos, nomeDoProduto) {
 //Verificação da funçao verificaProdutoNaLista
 console.log(verificaProdutoNaLista(produtosDisponiveis, "leite condensado"));
 console.log(verificaProdutoNaLista(produtosDisponiveis, "feijao"));
+*/
 
-//Para a função verificaProdutoNaLista, se eu tivesse mais de um produto na mesma categoria:
-/*
+//Para a função verificaProdutoNaLista, se eu tivesse mais de um produto na mesma categoria, imprime os objeto:
+
 function verificaProdutosNaLista(listaDeProdutos, nomeDoProduto) {
   let produtosEncontrado = [];
-  let lista;
 
   for (let produto of listaDeProdutos) {
     if (produto.categoria === nomeDoProduto) {
@@ -162,16 +181,73 @@ function verificaProdutosNaLista(listaDeProdutos, nomeDoProduto) {
     }
   }
 
-  if (produtosEncontrado == []) {
-    lista = alert("O produto desejado não foi encontrado na lista informada.");
+  if (produtosEncontrado.length === 0) {
+    alert("O produto desejado não foi encontrado na lista informada.");
   } else {
     for (prod of produtosEncontrado) {
-      lista = console.log(prod);
+      console.log(prod);
+    }
+    if (produtosEncontrado.length === 1) {
+      console.log(`Foi encontrado 1 resultado.`);
+    } else {
+      console.log(`Foram encontrados ${produtosEncontrado.length} resultados.`);
     }
   }
-  return lista;
+  return produtosEncontrado;
 }
-verificaProdutosNaLista(produtosDisponiveis, "detergente");
-//Não aparece o alert quando não tem o produto na lista
-verificaProdutosNaLista(produtosDisponiveis, "feijão");
+
+// verificaProdutosNaLista(produtosDisponiveis, "arroz");
+// verificaProdutosNaLista(produtosDisponiveis, "detergente");
+// verificaProdutosNaLista(produtosDisponiveis, "feijão");
+
+/*
+//TESTE (Retorna Lista)
+
+//Função para verificar se um produto está dentro de uma lista
+
+function verificaProdutoNaLista(listaDeProdutos, nomeDoProduto) {
+  let produtoEncontrado = [];
+
+  for (let produto of listaDeProdutos) {
+    if (produto.categoria.includes(nomeDoProduto)) {
+      produtoEncontrado.push(produto);
+    } else if (produto.categoria !== nomeDoProduto) {
+      produtoEncontrado = produtoEncontrado;
+    }
+  }
+
+  if (produtoEncontrado.length === 0) {
+    produtoEncontrado = alert(
+      "O produto desejado não foi encontrado na lista informada."
+    );
+  }
+
+  return produtoEncontrado;
+}
+//Verificação da funçao verificaProdutoNaLista
+// console.log(verificaProdutoNaLista(produtosDisponiveis, "leite condensado"));
+// console.log(verificaProdutoNaLista(produtosDisponiveis, "detergente"));
+// console.log(verificaProdutoNaLista(produtosDisponiveis, "feijão"));
+*/
+
+/*
+function verificaProdutosNaLista(listaDeProdutos, nomeDoProduto) {
+  let produtoEncontrado = [];
+    
+  for (let produto of listaDeProdutos) {
+    if (produto.categoria.includes(nomeDoProduto)) {
+      produtoEncontrado.push(produto);
+    } else if (produto.categoria.includes(nomeDoProduto) !== nomeDoProduto) {
+      produtoEncontrado = produtoEncontrado;
+    }
+  }
+
+  if (produtosEncontrado.length === 0) {
+    alert("O produto desejado não foi encontrado na lista informada.");
+  } else {
+    for (prodObj of produtoEncontrado) {
+      console.log(prodObj);
+    }
+  }
+}
 */
